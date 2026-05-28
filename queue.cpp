ea21f09612afd4de352f314de18ab8c238169348
Naufal Arif <na713634@gmail.com>
2026-05-28 18:59:29 +0700

    itteration from the start of the array until the rear

--- queue.cpp
+++ queue.cpp
@@ -113,4 +113,15 @@ public:
             }
 
             FRONT_Position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            cout << endl;
+        }
+    }
 };
