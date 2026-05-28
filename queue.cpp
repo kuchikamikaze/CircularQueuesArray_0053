56030fc437caea4294c02f6cd51c0a44e4822bca
Naufal Arif <na713634@gmail.com>
2026-05-28 18:46:44 +0700

    check if queue is full

--- queue.cpp
+++ queue.cpp
@@ -12,7 +12,7 @@ public:
         FRONT = -1;
         REAR = -1;
     }
-    
+
     void insert()
     {
         int num;
@@ -20,4 +20,13 @@ public:
         cout << "Enter a number : ";
         cin >> num;
         cout << endl;
+
+         // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "Queue overflow";
+            return;
+        }
+
+       
 };
