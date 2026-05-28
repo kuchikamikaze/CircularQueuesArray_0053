55f3f4216561f8f384f558797f2d41b1ba39c224
Naufal Arif <na713634@gmail.com>
2026-05-28 18:44:35 +0700

    public member constructor declaration

--- queue.cpp
+++ queue.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 class Queues{
-    private:
+private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
 };
